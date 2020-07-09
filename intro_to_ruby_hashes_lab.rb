137c9f7f924050460883b0e08402359c34c51102
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-09 05:08:43 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,8 +32,8 @@ def monopoly_with_third_tier
       :names => 
       {
         :reading_railroad => {},
-        :pennsylvania_railroad => {}
-        :b_and_o_railroad => {}
+        :pennsylvania_railroad => {},
+        :b_and_o_railroad => {},
         :shortline => {}
       },
       :rent_in_dollars => {
